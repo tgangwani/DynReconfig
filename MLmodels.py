a77936a70bd81d4b74cf184aff6e87f60343194c
tgangwani <tanmay2g@gmail.com>
2015-12-15 03:20:24 -0600

    initial files

---
+++ MLmodels.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import tensorflow as tf
+
+# options for each label - hardcoded!
+labelOpts = 3
+logDir = "/Users/tanmaygangwani/Desktop/CS446/Project/arch//code/scripts/tensorboardOut/"
+
+class softmax():
+  def __init__(self, importantFeatureLocs, identifier):
+    self.identifier = identifier # [int] to identify model 
+    self.importantFeatureLocs = importantFeatureLocs[:]  # copy (unordered) list
+    self.inputVecSize = len(importantFeatureLocs)
+    self.uniqueVals = list() # list of unique label values
+    self.iterations = 1000 # for stochastic training
+    self.batch_size = 100 # for stochastic training
+    self.x = tf.placeholder(tf.float32, [None, self.inputVecSize])
+    self.W = tf.Variable(tf.zeros([self.inputVecSize, labelOpts]))
+    self.b = tf.Variable(tf.zeros([labelOpts]))
+    self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b)
+    self.y_ = tf.placeholder(tf.float32, [None, labelOpts])
+    self.cross_entropy = -tf.reduce_sum(self.y_*tf.log(self.y))
+    self.train_step = tf.train.GradientDescentOptimizer(0.005).minimize(self.cross_entropy)
+    self.sess = tf.Session()
+
+    # summary op
+    self.ce_sum = tf.scalar_summary("cross entropy", self.cross_entropy)
+    #tf.histogram_summary("weights", self.W)
+    #tf.histogram_summary("biases", self.b)
+    
+    # tensor-flow summary
+    # we can't do a tf.merge_all_summaries() here because it will add summaries
+    # from other softmax objects also. We just need the local summaries
+    self.summary_op = self.ce_sum
+    tensorboardOut = logDir + str(self.identifier)
+    if not os.path.exists(tensorboardOut):
+      os.makedirs(tensorboardOut)
+    self.summary_writer = tf.train.SummaryWriter(tensorboardOut, graph_def=self.sess.graph_def)
+    
+    self.init = tf.initialize_all_variables()
+    self.sess.run(self.init)
+
+  def train(self, x_data, y_data):
+    
+    # y_data is a single column. It needs to be converted into a one-hot vector
+    # (y_data_oh). A one-hot vector is a vector which is 0 in most dimensions, 
+    # and 1 in a single dimension
+    y_data_oh = list(list())
+    self.uniqueVals = sorted(list(set(y_data)))
+    assert len(self.uniqueVals) == labelOpts
+
+    for data in y_data:
+      one_hot_entry = [int(data == self.uniqueVals[i]) for i in range(labelOpts)]
+      y_data_oh.append(one_hot_entry)
+
+    # one-hot vector
+    y_data_oh = np.asarray(y_data_oh)
+
+    # checks
+    # make sure we have enough input data to form a batch 
+    assert y_data_oh.shape[0] >= self.batch_size
+    assert y_data_oh.shape[0] == x_data.shape[0]
+
+    # using small batches of random data - stochastic training
+    for i in range(self.iterations):
+      sampleIds = np.random.choice(np.arange(y_data_oh.shape[0]), self.batch_size, replace=False)
+      x_sampled = np.take(x_data, sampleIds, axis=0)
+      y_sampled = np.take(y_data_oh, sampleIds, axis=0)
+
+      # periodic sampling (for stats)
+      if (None and i % 10 == 0):
+        # cross-entropy measurement on the whole training data
+        summary_str = self.sess.run(self.summary_op, feed_dict={self.x: x_data,
+          self.y_: y_data_oh})
+        self.summary_writer.add_summary(summary_str, i) 
+      else:
+        self.sess.run(self.train_step, feed_dict={self.x: x_sampled, self.y_: y_sampled})
+
+  def getLabelPrediction(self, x_test):
+    # feature selection using previously saved important locations
+    x_test_reduced = np.take(x_test, self.importantFeatureLocs, axis=1)
+
+    # 'labelOpts' x '#samples' matrix
+    labelWeights = self.sess.run(self.y, feed_dict={self.x: x_test_reduced})
+
+    # find the corresponding label using the uniqueVals list and return values
+    # for all samples
+    maxLabelIndices = np.argmax(labelWeights, axis=1)
+    maxLabels = [self.uniqueVals[i] for i in maxLabelIndices]
+    return np.asarray(maxLabels)
+
+  def getModelParams(self):
+    return (self.sess.run(self.W), self.sess.run(self.b))
+
+  def closeSession(self):
+    self.sess.close()
---
+++ learn.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import sys
+import numpy as np
+from sklearn import preprocessing
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+import MLmodels
+
+# 5 labels in data
+numlabels = 5
+reductionMethod = "RF" # RF (random forest) or LDA (linear discriminant analysis)
+RFThreshold = 0.9
+std_scale = None
+# list of objects of type ML model (e.g. softmax)
+modelObjs = list()
+
+# indices for important features in the feature vector
+importantFeatureLocs = list()
+
+# input data with features reduced using RF 
+x_data_rf_reduced = None
+
+# standardizing the columns - mean=0, variance=1
+def standardize():
+  global std_scale, x_data
+  std_scale = preprocessing.StandardScaler().fit(x_data)
+  x_data = std_scale.transform(x_data)
+
+def reduceRF(label):
+  global x_data_rf_reduced, importantFeatureLocs
+  model = ExtraTreesClassifier()
+  model.fit(x_data, y_data[:, label])
+
+  # the relative importance of each attribute
+  importance = model.feature_importances_
+  weight = float(0)
+  del importantFeatureLocs[:] # reset
+  #print(importance)  
+
+  for ele in np.sort(importance)[::-1]:
+    weight += float(ele)
+    featureIndex = np.where(importance==ele)
+    for loc in featureIndex[0]:
+      importantFeatureLocs.append(loc)
+  
+    if weight > RFThreshold :
+      break
+  
+  # remove duplications
+  importantFeatureLocs = list(set(importantFeatureLocs))
+
+  # extracting relevant columns from input data. Note that importantFeatureLocs
+  # may be unsorted (since python 'set' is unsorted), so features are extracted
+  # in unorderd fashion. This info is stored in the softmax model class
+  x_data_rf_reduced = x_data[:, importantFeatureLocs]
+
+# remove duplicate featureVecs
+def removeDuplicates():
+  global x_data, y_data
+  x_data = np.ascontiguousarray(x_data) 
+  uniques = np.unique(x_data.view([('', x_data.dtype)]*x_data.shape[1]),
+      return_index=True)[1]
+
+  # the list of duplicates is calculated by subtracting the list of uniques
+  # from the universe
+  duplicates = list(set(list(range(x_data.shape[0]))) - set(uniques))
+
+  print('Removing', len(duplicates), 'duplicates from (', x_data.shape[0], ') input data.')
+  x_data = np.delete(x_data, duplicates, axis=0)
+  y_data = np.delete(y_data, duplicates, axis=0)
+
+def printModelParams():
+  for modelObj in modelObjs:
+    w, b = modelObj.getModelParams()
+    print(w)
+    print(b)
+    print('\n')
+
+def testUnseen():
+  x_test = np.loadtxt(open('unseen.txt'), delimiter=",", skiprows=1, dtype=np.float64)
+  # standardize the test
+  x_test = std_scale.transform(x_test) 
+  y_pred = np.zeros((x_test.shape[0], numlabels), dtype=np.int)
+
+  # fill the prediction array by calling the model for each label separately
+  for modelObj in modelObjs:
+    index = modelObjs.index(modelObj)
+    y_pred[:, index] = modelObj.getLabelPrediction(x_test)
+
+  #np.set_printoptions(threshold=np.nan)
+  print(y_pred)
+
+if __name__=="__main__":
+  all_data = np.loadtxt(open('features.txt'), delimiter=",", skiprows=1, dtype=np.float64)
+  y_data = all_data[:, 0:numlabels]
+  x_data = all_data[:, numlabels:]
+  
+  standardize()
+  removeDuplicates()
+
+  for label in range(numlabels):
+    # feature reduction (done separately for each feature)
+    if reductionMethod == "RF":
+      reduceRF(label)
+      m = MLmodels.softmax(importantFeatureLocs, label)
+      m.train(x_data_rf_reduced, y_data[:, label])
+      modelObjs.append(m)
+    else:
+      print("[LDA] To be implemented")
+      sys.exit(1)
+      reduceLDA(label)
+
+  #printModelParams()
+
+  # get predictions for unseen samples
+  #testUnseen()
+
+  # close all TF sessions
+  for modelObj in modelObjs:
+    modelObj.closeSession()
